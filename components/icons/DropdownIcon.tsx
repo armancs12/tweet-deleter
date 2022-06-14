d9aafd020e15fd74d103f9574094a863c08e9f69
Serhat <47033919+serhatsdev@users.noreply.github.com>
2022-06-14 18:56:15 +0300

    Add Icons

---
+++ components/icons/DropdownIcon.tsx
@@ -0,0 +1,7 @@
+const DropdownIcon = () => (
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 10l5 5 5-5H7z" fill="#657281" />
+  </svg>
+);
+
+export default DropdownIcon;
