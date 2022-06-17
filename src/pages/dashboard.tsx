4fc3f5f01f8073f8d602df425a7f76c985f45f99
Serhat <47033919+serhatsdev@users.noreply.github.com>
2022-06-17 19:38:08 +0300

    Fix dashboard page layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -11,7 +11,7 @@ const DashboardPage: NextPage = () => {
       <div>
         <SearchFormContainer />
         <SearchFilters />
-        <section className="max-w-7xl mx-auto mt-6 grid grid-cols-1 lg:grid-cols-3 gap-12">
+        <section className="max-w-7xl mx-auto mt-6 grid grid-cols-1 lg:grid-cols-3 gap-12 pb-16">
           <div className="lg:col-start-1 lg:col-end-3 text-[17px] text-gray-500">
             <TweetsTable />
           </div>
