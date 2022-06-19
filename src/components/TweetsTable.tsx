39340d6fb8a402586cf2d8c90c33d4419062a58c
Serhat <47033919+serhatsdev@users.noreply.github.com>
2022-06-19 13:06:22 +0300

    Make tweets table dynamic

--- src/components/TweetsTable.tsx
+++ src/components/TweetsTable.tsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
 import Dropdown from "./Dropdown";
 import TweetsTableItem from "./TweetsTableItem";
 
+const tweets = [
+  {
+    id: "1",
+    content: "Here is a beautiful view https://t.co/M1x3GoJai6",
+    date: "Fri, Jun 17, 2022, 4:36 PM",
+    retweetCount: 0,
+    likeCount: 0,
+  },
+  {
+    id: "2",
+    content:
+      "RT @glanderco: FROG COMIC Art by @OpenAI DALL-E 2, text by Openai playground https://t.co/CBQT2P0tM6",
+    date: "Fri, Jun 17, 2022, 4:10 PM",
+    retweetCount: 31,
+    likeCount: 0,
+  },
+  {
+    id: "3",
+    content:
+      "RT @billboard: Drake keeps the surprises coming on his new album https://t.co/jDsR4AxHcB",
+    date: "Fri, Jun 17, 2022, 4:08 PM",
+    retweetCount: 36,
+    likeCount: 0,
+  },
+  {
+    id: "4",
+    content: "This is a test tweet as well",
+    date: "Fri, Jun 17, 2022, 2:19 PM",
+    retweetCount: 0,
+    likeCount: 0,
+  },
+];
+
 const TweetsTable = () => {
+  const [selectedTweets, setSelectedTweets] = useState<string[]>([]);
+
+  const toggleTweetSelect = (tweetId: string) => {
+    if (selectedTweets.includes(tweetId)) {
+      setSelectedTweets(selectedTweets.filter((id) => id !== tweetId));
+    } else {
+      setSelectedTweets([...selectedTweets, tweetId]);
+    }
+  };
+
+  const selectAllTweets = () => {
+    setSelectedTweets(tweets.map((tw) => tw.id));
+  };
+
+  const deselectAllTweets = () => {
+    setSelectedTweets([]);
+  };
+
   return (
     <div className="border border-gray-300 rounded">
       <div>
@@ -9,16 +61,32 @@ const TweetsTable = () => {
           <div className="flex-1 text-left">
             <Dropdown
               items={[
-                { type: "button", label: "Select shown (3 items)" },
-                { type: "button", label: "Select all (3 items)" },
-                { type: "button", label: "Deselect all" },
+                {
+                  type: "button",
+                  label: `Select shown (${tweets.length} items)`,
+                  onClick: selectAllTweets,
+                },
+                {
+                  type: "button",
+                  label: `Select all (${tweets.length})`,
+                  onClick: selectAllTweets,
+                },
+                {
+                  type: "button",
+                  label: "Deselect all",
+                  onClick: deselectAllTweets,
+                },
               ]}
             >
-              <input type="checkbox" className="w-4 h-4" />
+              <input
+                type="checkbox"
+                className="w-4 h-4"
+                checked={selectedTweets.length > 0}
+              />
             </Dropdown>
           </div>
           <div className="flex-1 text-center text-xs text-gray-500 hidden lg:block">
-            Showing 3 of 3 entries
+            {`Showing ${tweets.length} of ${tweets.length} entries`}
           </div>
           <div className="flex-1 text-right">
             <select className="text-sm font-semibold text-gray-500 bg-white text-center">
@@ -35,13 +103,18 @@ const TweetsTable = () => {
           Showing 3 of 3 entries
         </div>
         <div>
-          <TweetsTableItem />
-          <TweetsTableItem />
-          <TweetsTableItem />
-          <TweetsTableItem />
-          <TweetsTableItem />
-          <TweetsTableItem />
-          <TweetsTableItem />
+          {tweets.map((tweet) => (
+            <TweetsTableItem
+              key={tweet.id}
+              id={tweet.id}
+              content={tweet.content}
+              date={tweet.date}
+              likeCount={tweet.likeCount}
+              retweetCount={tweet.retweetCount}
+              onCheckboxToggle={() => toggleTweetSelect(tweet.id)}
+              isSelected={selectedTweets.includes(tweet.id)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/TweetsTableItem.tsx
+++ src/components/TweetsTableItem.tsx
@@ -1,12 +1,57 @@
+import clsx from "clsx";
+import { FC } from "react";
 import ExternalLinkIcon from "./icons/ExternalLinkIcon";
 import TwitterLikeIcon from "./icons/TwitterLikeIcon";
 import TwitterRetweetIcon from "./icons/TwitterRetweetIcon";
 
-const TweetsTableItem = () => {
+type Props = {
+  id: string;
+  content: string;
+  date: string;
+  retweetCount: number;
+  likeCount: number;
+  isSelected?: boolean;
+  onCheckboxToggle: () => void;
+};
+
+const classes = {
+  base: {
+    root: "relative flex items-center p-4 border-t border-gray-300 transition-colors duration-200 ease-out",
+    border:
+      "absolute left-0 h-full w-1 bg-blue-500 transition-opacity opacity-0 duration-200 ease-out",
+  },
+  selected: {
+    root: "bg-gray-100",
+    border: "opacity-100",
+  },
+};
+
+const TweetsTableItem: FC<Props> = ({
+  id,
+  content,
+  date,
+  likeCount,
+  retweetCount,
+  onCheckboxToggle,
+  isSelected = false,
+}) => {
   return (
-    <div className="relative flex items-center p-4 border-t border-gray-300 transition-colors duration-200 ease-out">
+    <div
+      className={clsx(classes.base.root, isSelected && classes.selected.root)}
+    >
+      <div
+        className={clsx(
+          classes.base.border,
+          isSelected && classes.selected.border
+        )}
+      ></div>
       <div>
-        <input type="checkbox" className="mr-4" />
+        <input
+          type={"checkbox"}
+          className="mr-4 w-4 h-4 p-1 cursor-pointer"
+          checked={isSelected}
+          onChange={onCheckboxToggle}
+        />
       </div>
       <div className="flex-grow">
         <div>
@@ -22,19 +67,17 @@ const TweetsTableItem = () => {
             <div className="hidden lg:flex items-center">
               <span className="flex items-center text-sm mr-4">
                 <TwitterRetweetIcon />
-                <span>0</span>
+                <span>{retweetCount}</span>
               </span>
               <span className="flex items-center text-sm mr-4">
                 <TwitterLikeIcon />
-                <span>0</span>
+                <span>{likeCount}</span>
               </span>
-              <span className="text-sm">Fri, Jun 17, 2022, 2:19 PM</span>
+              <span className="text-sm">{date}</span>
             </div>
           </div>
           <div className="mt-3">
-            <span className="text-[17px] text-gray-500">
-              This is a test tweet as well.
-            </span>
+            <span className="text-[17px] text-gray-500">{content}</span>
           </div>
         </div>
       </div>
