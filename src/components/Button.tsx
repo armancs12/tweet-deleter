692575061341c347d3a5b404c90c7a5144aa47b9
Serhat <47033919+serhatsdev@users.noreply.github.com>
2022-06-19 16:18:50 +0300

    Update button types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,8 @@ const classes = {
   types: {
     primary:
       "text-white bg-[rgb(29,161,242)] hover:bg-[rgb(12,133,207)] active:bg-[rgb(9,102,159)]",
+    "primary-inverted":
+      "text-[rgb(29,161,242)] bg-white hover:bg-gray-100 active:bg-gray-200",
     secondary:
       "border border-gray-300 bg-gray-100 text-blue-400 hover:bg-gray-200 active:bg-gray-300",
   },
